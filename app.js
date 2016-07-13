a6e307846a398edcb4fbff2e820484e158ac0f68
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-07-13 07:25:48 -0500

    add models

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+const MONGO_PASS = process.env['MONGO_DB_PASS'];
+const MONGO_HOST = process.env['$OPENSHIFT_MONGODB_DB_HOST'];
+const MONGO_PORT = process.env['$OPENSHIFT_MONGODB_DB_PORT'];
+mongoose.connect(`mongodb://admin:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/nodejs`);
+
 var routes = require('./routes/index');
+var courses = require('./routes/courses');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -18,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/', routes);
+app.use('/courses', courses);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
