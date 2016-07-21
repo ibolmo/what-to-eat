7e05c4c76eae3acd2e5299c3b30da5bb7c128e69
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-07-21 12:04:10 -0500

    add gulp-nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 
-gulp.task('default', function() {
-  console.log('yey task!');
+gulp.task('start', function () {
+
+  nodemon({
+    script: './bin/www',                // file that starts server
+    ext: 'js',                          // which files to restart server
+    env: { 'NODE_ENV': 'development' }  // environment variables
+  });
+
 });
+
+gulp.task('default', ['start']);
