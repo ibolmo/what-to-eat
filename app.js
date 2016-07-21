cbb26bdca3f15483fdbe7bb18fed8650f4409137
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-07-21 10:13:50 -0500

    added livereload and nodemon

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var livereload = require('express-livereload');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/boot');
@@ -13,6 +14,9 @@ var users = require('./routes/users');
 var courses = require('./routes/courses');
 
 var app = express();
+livereload(app, {
+  watchDir: '.'
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
